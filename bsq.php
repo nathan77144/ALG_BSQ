f2054ba2613f83c093b8168e6b133c47a6c6833e
nathan77144 <nathan.gerard@epitech.eu>
2022-11-08 08:51:17 +0100

    largeur maximal du carré et debut de la hauteur

--- bsq.php
+++ bsq.php
@@ -1,4 +1,10 @@
 <?php
+
+class coordonne
+{
+    public $x;
+    public $y;
+}
 // je lis mon fichier
 $ouvrir = fopen($argv[1], "r");
 //je lis la premiere ligne pour connaitre le nombre de ligne de mon fichier
@@ -6,25 +12,46 @@ $nbligne = fgets($ouvrir);
 
 var_dump($nbligne);
 // je lis tout le fichier 
- $lire = fread($ouvrir , filesize($argv[1])); 
+$lire = fread($ouvrir, filesize($argv[1]));
 
 var_dump($lire);
 //je regarde quand est ce que j'arrive sur un retour a la ligne pour definir la taille d'une ligne
-$largeurligne = strpos($lire,"\n")+1;
+$largeurligne = strpos($lire, "\n") + 1;
 
 var_dump($largeurligne);
 // je cherche la position des o dans mon fichier a partir de la ligne 2
-$cherchezero = preg_match_all('#o#', $lire, $matches, PREG_OFFSET_CAPTURE);
+$nbzero = preg_match_all('#o#', $lire, $matches, PREG_OFFSET_CAPTURE);
 
-var_dump($cherchezero);
 var_dump($matches);
 
-foreach($o as $value){
-    
-    if("o" == $o){
-        
+$tableauxcoor = array();
+//initialisation a zero de mon tableaux des coordonnee des obstacles
+for ($i = 0; $i < $nbzero; $i++) {
 
-    }
+    $tableauxcoor[$i] = new coordonne;
+}
 
+var_dump("hfhjfjjf" ,$tableauxcoor);
+//je fais le calcul de mes coordonnée des obstacles pour tout le plateau
+for($i = 0 ; $i < $nbzero ; $i++){
+
+    $tableauxcoor[$i]->x=fmod($matches[0][$i][1] , $largeurligne) ;
+    $tableauxcoor[$i]->y=(int)($matches[0][$i][1] / $largeurligne);
 
 }
+
+for ($i = 0; $i < $nbzero; $i++){
+// je cherche la largeur maximal du carré
+if($tableauxcoor[$i + 1]->y == $tableauxcoor[$i]){
+
+    $largeurmax= $tableauxcoor[$i + 1]->x - ($tableauxcoor[$i ]->x)-2;
+}
+else $largeurmax = $largeurligne - $tableauxcoor[$i]->x -3;
+//je cherche la hauteur maximale du carré
+
+
+    
+
+}
+
+var_dump("toto", $tableauxcoor);
